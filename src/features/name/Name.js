53ff89ef0c130fa3aa8e73c9289902bddd5faf5d
Fourcade Antoine <a.fourcade65@gmail.com>
2020-10-15 10:31:50 +0200

    Setup du projet
    Création des premiers composants

---
+++ src/features/name/Name.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Text } from 'grommet'
+
+const Name = () => {
+    return (
+        <Box>
+            <Text>
+                Antoine
+            </Text>
+        </Box>
+    )
+}
+
+export default Name
