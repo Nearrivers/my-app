280c491b18cecff935aa5a4e51b8b535f8a66a56
Fourcade Antoine <a.fourcade65@gmail.com>
2020-10-22 08:50:17 +0200

    Affichage centres d'intérêt terminé

--- src/features/interet/Interet.js
+++ src/features/interet/Interet.js
@@ -1,35 +1,60 @@
 import React from 'react';
-import { Box, Carousel, Image } from 'grommet';
+import { Box, Image, Stack, Card, CardHeader, CardBody, Heading } from 'grommet';
 
 
 const sources = [
     {
         type: "Code",
-        image: require("../../img/interets/code.jpg")
+        image: require("../../img/interets/code.jpg"),
+        footer: "Programmation"
     },
     {
         type: "Dessin",
-        image: require("../../img/interets/dessin.jpg")
+        image: require("../../img/interets/dessin.jpg"),
+        footer: "Dessin"
     },
     {
         type: "Games",
-        image: require("../../img/interets/games.jpg")
+        image: require("../../img/interets/games.jpg"),
+        footer: "Jeux vidéos"
     },
     {
         type: "Musculation",
-        image: require("../../img/interets/musculation.jpg")
+        image: require("../../img/interets/musculation.jpg"),
+        footer: "Musculation"
     }
 ]
 
-const Interet = ({ controls, ...rest }) => (
-    <Box align="center" pad="large" fill>
-        <Carousel controls={controls} {...rest}>
-            {sources.map((value,i) => (
-                <Box pad="medium" background="accent-1" key={i}>
-                    <Image fit="cover" src={value.image} width="medium" height="medium"/>
-                </Box>
-            ))}
-        </Carousel>
+const Interet = () => (
+    <Box direction="row-responsive" align="center" pad="large" gap="small" fill>        
+        {sources.map((value,i) => (
+            <Card width="medium" key={value.type}>
+            {/* Stacked CardBody and CardHeader on top of each other 
+            in that order */}
+                <Stack anchor="bottom-left">
+                    <CardBody height="medium">
+                        <Image
+                            fit="cover"
+                            src={value.image}
+                            a11yTitle={value.type}
+                        />
+                    </CardBody>
+                    <CardHeader
+                        pad={{ horizontal: 'small', vertical: 'small' }}
+                        // https://gist.github.com/lopspower/03fb1cc0ac9f32ef38f4#all-hex-value-from-100-to-0-alpha
+                        background="#000000A0"
+                        width="medium"
+                        justify="start"
+                    >
+                        <Box>
+                            <Heading level="3" margin="none">
+                            {value.footer}
+                            </Heading>
+                        </Box>
+                    </CardHeader>
+                </Stack>
+            </Card>
+        ))}
     </Box>
 )
 
